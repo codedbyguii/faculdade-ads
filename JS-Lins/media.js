1a8e781ea417358a9a66873cea90698c504272e0
codedbyguii <guylhermenascimento2006@gmail.com>
2025-08-28 20:51:03 -0300

    novas atividades.

---
+++ JS-Lins/media.js
@@ -0,0 +1,13 @@
+
+const notaFinal = 7;
+const faltas = 3;
+
+// se a nota for menor que 7 e as faltas forem maiores que 4, o aluno é reprovado!
+// se não, o aluno é aprovado
+
+
+if (notaFinal < 7 || faltas > 4) {
+    console.log("reprovado, noas festas");
+} else {
+    console.log("aprovado, boas festas");
+}
