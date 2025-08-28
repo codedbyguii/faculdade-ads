42517e595cb2946749d7c721f14547bd57640596
codedbyguii <guylhermenascimento2006@gmail.com>
2025-08-28 08:24:06 -0300

    primeira commit

---
+++ JS-Lins/app.js
@@ -0,0 +1,9 @@
+// olá mundo com upper e lower case.
+
+
+let frase = "olá, mundo!";
+let frase2 = "OLÁ, MUNDO!";
+
+
+console.log("com uppercase: " + frase.toLocaleUpperCase());
+console.log("com lowercase: " + frase2.toLocaleLowerCase());
