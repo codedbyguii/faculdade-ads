773c325f21e28e9121ee3fb3002aea8c8bdd6c9a
codedbyguii <guylhermenascimento2006@gmail.com>
2025-09-03 18:48:00 -0300

    Lins - Atividade

---
+++ JS-Lins/indexof.js
@@ -0,0 +1,14 @@
+const alunos = ['Guilherme', 'Sarah', 'Tainá']
+
+
+function receberAluno(aluno) {
+    if (alunos.includes(aluno)) {
+        let media = medias(alunos.indexOf(alunos))
+        return media
+    }
+    return 'aluno nao existe'
+}
+
+
+
+console.log(receberAluno('Guilherme'))
